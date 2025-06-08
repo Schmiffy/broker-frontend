14539db0e5d108630264f9475995403b5374a3f6
Michael Schmidt <michaelschmidt@Michaels-Laptop.local>
2025-06-08 10:09:08 +0700

    adjust   base: './',

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // The base path should be the root for S3 deployments.
-  base: '/', 
+  base: './', 
   plugins: [react()],
 })
